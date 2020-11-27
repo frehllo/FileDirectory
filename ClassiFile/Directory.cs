b132a93a06dd800078e4ed8c54135a82815c0bef
narcisbuzatu2003@gmail.com <narcisbuzatu2003@gmail.com>
2020-11-27 14:07:14 +0100

    Aggiungere i file di progetto.

---
+++ ClassiFile/Directory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassiFile
+{
+    public class Directory : FileAstratto
+    {
+        List<File> files = new List<File>();
+        public Directory(string nome, Directory directory) : base(nome, directory) { }
+
+        public void AddFile(File file)
+        {
+            files.Add(file);
+        }
+
+        public List<File> GetFiles()
+        {
+            return files;
+        }
+
+        public override long GetDimensione()
+        {
+            long somma = 0;
+            foreach (File file in files)
+            {
+                somma += file.GetDimensione();
+            }
+            return somma;
+        }
+    }
+}
---
+++ ClassiFile/FileAstratto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ClassiFile
+{
+    public abstract class FileAstratto
+    {
+        public string Nome { get; private set; }
+        public FileAstratto (string nome, Directory directory)
+        {
+            Nome = nome;
+        }
+
+        public abstract long GetDimensione();
+        public string Stampa()
+        {
+
+        }
+    }
+}
